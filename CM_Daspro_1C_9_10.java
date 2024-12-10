d2f3cd348d4a2a8f3e682780772e8f49d5aaea53
Fahrulkz <fahrulzaki10@gmail.com>
2024-12-10 15:34:07 +0700

    membuat variabel

--- CM_Daspro_1C_9_10.java
+++ CM_Daspro_1C_9_10.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class CM_Daspro_1C_9_10 {
 
+    static String[][] inventori = new String[10][2];
+    static {
+        inventori[0] = new String[] {"Kopi Hitam", "Minuman"};
+        inventori[1] = new String[] {"Capucino", "Minuman"};
+        inventori[2] = new String[] {"Teh Tarik", "Minuman"};
+    }
+    static int[] stok = new int[10];
+    static {
+        stok[0] = 10;
+        stok[1] = 5;
+        stok[2] = 8;
+    }
     public static void main(String[] args) {
         
     }

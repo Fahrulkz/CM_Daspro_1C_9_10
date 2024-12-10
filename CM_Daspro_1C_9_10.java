e2345f3e07e67bd8ad2dbc70066ec77eabe09320
Fahrulkz <fahrulzaki10@gmail.com>
2024-12-10 15:43:18 +0700

    Mengisi_listMenu

--- CM_Daspro_1C_9_10.java
+++ CM_Daspro_1C_9_10.java
@@ -14,11 +14,41 @@ public class CM_Daspro_1C_9_10 {
         stok[2] = 8;
     }
     public static void main(String[] args) {
-        
+
     }
 
     static void listMenu(){
+        Scanner sc = new Scanner(System.in);
+        int pilihan;
 
+        do {
+            System.out.println("\n======= Menu Inventori Cafe =======");
+            System.out.println("1. Tampilkan Inventori");
+            System.out.println("2. Tambah Stok untuk item yang ada");
+            System.out.println("3. Tambah Item Baru");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih Menu: ");
+            pilihan = sc.nextInt();
+            sc.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    tampilkanData();
+                    break;
+                case 2:
+                    tambahStock();
+                    break;
+                case 3:
+                    tambahItem();
+                    break;
+                case 4:
+                    System.out.println("Terimakasih Program Selesai:)");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Coba lagi!.");
+            }
+        } while (pilihan != 4);
+        sc.close();
     }
 
     static void tampilkanData() {

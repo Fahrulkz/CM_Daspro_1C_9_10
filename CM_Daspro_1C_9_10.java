6afe048e1cdde0f1e36abcba573eaed386846a61
gunawan885 <gunawanwi376@gmail.com>
2024-12-10 15:26:36 +0700

    Memulai dan menambahkan fungsi

---
+++ CM_Daspro_1C_9_10.java
@@ -0,0 +1,22 @@
+public class CM_Daspro_1C_9_10 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    static void listMenu(){
+
+    }
+
+    static void tampilkanData() {
+
+    }
+
+    static void tambahItem(){
+
+    }
+
+    static void tambahStock(){
+        
+    }
+}
